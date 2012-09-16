3d68e19d78cae6c2d1dd9a375e7446397f67f51a
TNU <tian_yu92@hotmail.com>
2012-09-15 23:52:01 -0400

    Tokenizer works as expected

--- Tokenizer.hs
+++ Tokenizer.hs
@@ -4,49 +4,48 @@ module Tokenizer (
 ) where
 
 import Data.List (span)
+import Control.Monad (liftM2)
 
 data Token = INT Integer 
            | ID String
            | OP Char Int 
            deriving (Show)
            
+isNL x  = x `elem` "\n\r"
+isSP x  = x `elem` " \f\t\v"
+isNum x = x `elem` ['0'..'9']
+isId x  = x `elem` (['a'..'z'] ++ ['A'..'Z'])
+isOP x  = x `elem` "+-*/"
+           
 tokenize :: (Monad m) => String -> [m [Token]]
 tokenize [] = [(return [])]
 tokenize (x:xs) 
     | isNL x = ((return []) : (tokenize xs))
     | isSP x = tokenize xs
-               
-tokenize str = do
-            (token, rest) <- getFirstToken str
-            curLineTokens <- (head (tokenize rest))
-            (return (token:curLineTokens)) : (tail (tokenize rest))
-            
-isNL x  = x `elem` "\n\r"
-isSP x  = x `elem` " \f\t\v"
-
-getFirstToken :: (Monad m) => String -> m (Token, String)
-getFirstToken str@(x:xs)
-    | isNum x   = return $ getNum str
-    | isId x    = return $ getId str
-    | isOP x    = return $ getOp str
-    | otherwise = error ("TOKENIZE: umatched token '" ++ (show x) ++ "'")
-
-isNum x = x `elem` ['0'..'9']
-isId x  = x `elem` (['a'..'z'] ++ ['A'..'Z'])
-isOP x  = x `elem` "+-*/"
+tokenize str = newCurLine : otherLines
+            where (token, rest) = getOneToken str
+                  (curLine:otherLines) = tokenize rest
+                  newCurLine = liftM2 (:) token curLine
+                  
+getOneToken :: (Monad m) => String -> (m Token, String)
+getOneToken str@(x:xs)
+    | isNum x   = getNum str
+    | isId x    = getId str
+    | isOP x    = getOp str
+    | otherwise = (error ("TOKENIZE: umatched token '" ++ (show x) ++ "'"), xs)
           
-getNum :: String -> (Token, String)
-getNum str = (INT (read numStr), rest)
+getNum :: (Monad m) => String -> (m Token, String)
+getNum str = (return . INT . read $ numStr, rest)
              where (numStr, rest) = span isNum str
              
-getId :: String -> (Token, String)
-getId (x:xs) = (ID (x:idTail), rest)
+getId :: (Monad m) => String -> (m Token, String)
+getId (x:xs) = (return (ID (x:idTail)), rest)
                where (idTail, rest) = span isNumOrId xs
                      isNumOrId x = (isNum x) || (isId x)
                 
-getOp :: String -> (Token, String)
+getOp :: (Monad m) => String -> (m Token, String)
 getOp (x:rest)
-    | x `elem` "+-" = (OP x 1, rest)
-    | x `elem` "*/" = (OP x 2, rest)
+    | x `elem` "+-" = (return (OP x 1), rest)
+    | x `elem` "*/" = (return (OP x 2), rest)
           
       
